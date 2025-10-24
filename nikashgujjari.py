c314bc7a949bd88e2aea0895c8634156ae37770a
Nikash Gujjari <nikashgujjari@gmail.com>
2025-10-24 14:12:30 -0400

    Minor Edit

--- nikashgujjari.py
+++ nikashgujjari.py
@@ -10,5 +10,5 @@ class BankAccount:
     def overdrawn(self):
         return self.balance < 0
 my_account = BankAccount(15)
-my_account.withdraw(110)
+my_account.withdraw(120)
 print (my_account.balance, my_account.overdrawn())
