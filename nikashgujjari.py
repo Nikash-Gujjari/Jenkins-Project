8aa78e8b7cebba0d0cfd297cc157de99dd47b52f
Nikash Gujjari <nikashgujjari@gmail.com>
2025-10-24 14:15:57 -0400

    Withdraw set to 150

--- nikashgujjari.py
+++ nikashgujjari.py
@@ -10,5 +10,5 @@ class BankAccount:
     def overdrawn(self):
         return self.balance < 0
 my_account = BankAccount(15)
-my_account.withdraw(120)
+my_account.withdraw(150)
 print (my_account.balance, my_account.overdrawn())
